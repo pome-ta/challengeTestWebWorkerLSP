a975e497adbd2634abf40522fc852b3b996ae601
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-11-23 22:24:51 +0900

    v0.0.2.1

---
+++ docs/js/test/v0.0.2/worker-ready.test.js
@@ -0,0 +1,29 @@
+// test/v0.0.2/worker-ready.test.js
+// v0.0.2.1
+
+import { expect } from 'chai';
+import {
+  createTestWorker,
+  waitForWorkerReady,
+  addResult,
+} from './test-utils.js';
+
+(async () => {
+  const testName = 'Worker: should send worker/ready notification on startup';
+  let worker;
+  let logs = [];
+
+  try {
+    worker = createTestWorker('./js/worker.js', (log) => logs.push(log));
+    await waitForWorkerReady(worker);
+    const readyLog = logs.find((log) =>
+      log.includes('Worker loaded and ready')
+    );
+    expect(readyLog).to.exist;
+    addResult(testName, true);
+  } catch (error) {
+    addResult(testName, false, error.message);
+  } finally {
+    worker?.terminate();
+  }
+})();
