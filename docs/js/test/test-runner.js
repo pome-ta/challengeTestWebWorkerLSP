be7dccf1b8fa4df11f5a58649498ee08464abd98
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-12-05 12:31:36 +0900

    v0.0.3.0

--- docs/js/test/test-runner.js
+++ docs/js/test/test-runner.js
@@ -1,15 +1,7 @@
 // test/test-runner.js
-// v0.0.2.14
+// v0.0.3.0
 
 console.log('🚀 test-runner.js loaded');
 
-//import './v0.0.2/worker-ready.test.js';
-//import './v0.0.2/worker-vfs-init.test.js';
-//import './v0.0.2/worker-vfs-cached-init.test.js';
-//import './v0.0.2/worker-lsp-initialize.test.js';
-//import './v0.0.2/worker-lsp-diagnostics.test.js';
-//import './v0.0.2/worker-vfs-file.test.js';
-//import './v0.0.2/worker-lsp-multi-file.test.js';
-//import './v0.0.2/worker-diagnostics-flatten.test.js';
-import './v0.0.2/worker-didChange-basic.test.js';
-//import './v0.0.2/worker-didChange-incremental-min.test.js';
+import './v0.0.3/vfs-ensureReady.basic.test.js';
+
---
+++ docs/js/test/v0.0.3/vfs-ensureReady.basic.test.js
@@ -0,0 +1,30 @@
+// test/v0.0.3/vfs-ensureReady.basic.test.js
+// v0.0.3.0
+
+import { expect } from 'chai';
+import {
+  createTestWorker,
+  waitForWorkerReady,
+  sendRequest,
+  addResult,
+} from './test-utils.js';
+
+console.log('🧩 vfs-ensureReady.basic.test loaded');
+
+(async () => {
+  const testName = 'VFS: ensureReady should complete without error';
+  let worker;
+  try {
+    worker = createTestWorker('./js/worker.js');
+
+    await waitForWorkerReady(worker);
+    await sendRequest(worker, 'vfs/ensureReady');
+
+    // no exception -> success
+    addResult(testName, true);
+  } catch (err) {
+    addResult(testName, false, err.message);
+  } finally {
+    worker?.terminate();
+  }
+})();
