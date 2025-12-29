dc58467f3deb13b58c3627837307b0c6f316652f
pome-ta <53405097+pome-ta@users.noreply.github.com>
2025-12-29 17:02:41 +0900

    comment out

--- docs/js/test/test-runner.js
+++ docs/js/test/test-runner.js
@@ -43,10 +43,12 @@ import './v0.0.3/phase6-multifile-isolation.test.js';
 */
 
 // v0.0.3.8
+
 import './v0.0.3/phase7-incremental-sync.test.js';
 import './v0.0.3/phase7-version-increment.test.js';
 import './v0.0.3/phase7-diagnostics.test.js';
 import './v0.0.3/phase7-before-initialize.test.js';
+
 
 // v0.0.3.9
 import './v0.0.3/phase8-completion-hover.test.js';
@@ -54,5 +56,4 @@ import './v0.0.3/phase8-completion-hover.test.js';
 
 // v0.0.3.10
 import './v0.0.3/phase9-completion-hover.test.js';
-
 
